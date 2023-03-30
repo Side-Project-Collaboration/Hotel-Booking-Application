e75c5f10b84afa355a73e9cae6781546d397d6e7
Phakamani <mileighndlovu@gmail.com>
2023-03-30 21:56:11 +0200

    adding colorama

--- search-prompt/generate.calendar.py
+++ search-prompt/generate.calendar.py
@@ -15,10 +15,21 @@
 # print("SUPPOSE ALL ROOMS ARE AVAIABLE")
 
 import calendar
+from colorama import init, Fore, Style
+
+# Initialize colorama
+init()
 
 # Create a TextCalendar object for the month of April 2023
 cal = calendar.TextCalendar(calendar.MONDAY)
+
+# Generate the calendar as a string
 calendar_text = cal.formatmonth(2023, 4)
 
-# Print the calendar
+# Add a red marker for April 24, 2023
+marker_text = f"{Fore.RED}24{Style.RESET_ALL}"
+calendar_text = calendar_text.replace(' 24 ', f'{marker_text}')
+
+# Print the modified calendar
 print(calendar_text)
+
